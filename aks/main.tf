5aa3093320a4d3e8fc143b616c9b899f1c5f790e
sergeyd <sergey.dziuban@intapp.com>
2021-09-19 15:37:44 +0300

    rebalance AKS cluster

--- aks/main.tf
+++ aks/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_B2ms"
   }
 
   identity {
@@ -37,3 +37,16 @@ resource "azurerm_kubernetes_cluster" "default" {
     environment = "Demo"
   }
 }
+
+resource "azurerm_kubernetes_cluster_node_pool" "default" {
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.default.id
+  vm_size               = "Standard_D4s_v3"
+  node_count            = 1
+
+  tags = {
+    Environment = "Demo"
+  }
+}
+
+
--- aks/outputs.tf
+++ aks/outputs.tf
@@ -6,6 +6,15 @@ output "kubernetes_cluster_name" {
   value = azurerm_kubernetes_cluster.default.name
 }
 
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.default.kube_config_raw
+  sensitive = true
+}
+
 # output "host" {
 #   value = azurerm_kubernetes_cluster.default.kube_config.0.host
 # }
