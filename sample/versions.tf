b9a673abfa174fb919a01533e948bb0fd7a4d0a7
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-04-23 00:37:56 +0300

    skip

--- sample/versions.tf
+++ sample/versions.tf
@@ -12,12 +12,12 @@ terraform {
 
   }
 
-  backend "azurerm" {
-    resource_group_name  = "iac-poc-terraform"
-    storage_account_name = "iacpocterraform"
-    container_name       = "terraform"
-    key                  = "iac-poc/terraform/terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "iac-poc-terraform"
+  #   storage_account_name = "iacpocterraform"
+  #   container_name       = "terraform"
+  #   key                  = "iac-poc/terraform/terraform.tfstate"
+  # }
 
   required_version = ">= 0.14.9"
 }
