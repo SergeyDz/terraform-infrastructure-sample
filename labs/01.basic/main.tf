c96bb3cdb87015e9c19d9c6800cf8cf89367e1fa
Sergey Dzyuban <dzyuban@gmail.com>
2022-06-18 15:45:54 +0000

    new subnet

--- labs/01.basic/main.tf
+++ labs/01.basic/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "vm" {
   ami           = "ami-0cff7528ff583bf9a"
-  subnet_id     = "subnet-04206cc6315f87c35"
+  subnet_id     = "subnet-014a79b070ccb260d"
   instance_type = "t3.micro"
   tags = {
     Name = "my-first-tf-node"
