0f379915c1f1f201e298729e4544cf7cfb85b46c
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-06-18 14:19:01 +0300

    add install

---
+++ scripts/tools.install.sh
@@ -0,0 +1,4 @@
+curl -L https://raw.githubusercontent.com/warrensbox/terraform-switcher/release/install.sh | bash
+curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
+pip install checkov
+curl -fsSL https://raw.githubusercontent.com/infracost/infracost/master/scripts/install.sh | bash
