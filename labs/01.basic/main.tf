0ea618dd3970d9b4f1f538aabc7f3f6de68706c9
Sergey Dzyuban <sergey.dziuban@intapp.com>
2022-06-25 22:22:40 +0300

    output added

--- labs/01.basic/main.tf
+++ labs/01.basic/main.tf
@@ -5,7 +5,5 @@ resource "aws_instance" "vm" {
   ami           = var.ami
   subnet_id     = var.subnet_id
   instance_type = var.instance_type
-  tags = {
-    Name = var.machine_name
-  }
+  tags = var.tags
 }
